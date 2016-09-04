7c68a3e7b1df91f8eee905aa999e7e86dea0cac5
Bernard Boguszewski <bernardboguszewski@gmail.com>
2016-09-04 12:35:14 +0200

    AdminPanel + Requests

---
+++ src/main/java/pl/com/bottega/cinema/api/AdminPanel.java
@@ -0,0 +1,19 @@
+package pl.com.bottega.cinema.api;
+
+/**
+ * Created by bernard.boguszewski on 04.09.2016.
+ */
+public class AdminPanel {
+
+    public void createCinema(CreateCinemaRequest request){
+
+    }
+
+    public void crateMovie(CreateMovieRequest request){
+
+    }
+
+    public void createShow(CreateShowsRequest request){
+
+    }
+}
