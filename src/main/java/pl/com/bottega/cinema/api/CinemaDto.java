50521245a40f32838528a7b7917b543b696d3aa0
Bernard Boguszewski <bernardboguszewski@gmail.com>
2016-09-04 13:51:24 +0200

    CinemaCatalog & CinemaDto

---
+++ src/main/java/pl/com/bottega/cinema/api/CinemaDto.java
@@ -0,0 +1,11 @@
+package pl.com.bottega.cinema.api;
+
+/**
+ * Created by bernard.boguszewski on 04.09.2016.
+ */
+public class CinemaDto {
+
+    private Long id;
+    private String name;
+    private String city;
+}
--- src/main/java/pl/com/bottega/cinema/ui/ListAllCinemasResponse.java
+++ src/main/java/pl/com/bottega/cinema/ui/ListAllCinemasResponse.java
@@ -1,7 +1,13 @@
 package pl.com.bottega.cinema.ui;
 
+import pl.com.bottega.cinema.api.CinemaDto;
+
+import java.util.Collection;
+
 /**
  * Created by anna on 04.09.2016.
  */
 public class ListAllCinemasResponse {
+
+    Collection<CinemaDto> cinemas;
 }
