ec0d52c5caf4a6ad9ee2b42ad1f53a9147dc97d1
anna <krzysztoszek.a@gmail.com>
2016-09-04 13:20:07 +0200

    error handler

--- src/main/java/pl/com/bottega/cinema/api/InvalidRequestException.java
+++ src/main/java/pl/com/bottega/cinema/api/InvalidRequestException.java
@@ -3,7 +3,7 @@ package pl.com.bottega.cinema.api;
 /**
  * Created by bernard.boguszewski on 04.09.2016.
  */
-public class InvalidRequestException {
+public class InvalidRequestException extends RuntimeException {
 
     private String error;
 }
