4072c6508a9be81e7af7600a659eb3b44f82313b
Admin <liravanigroup@gmail.com>
2016-09-04 15:12:22 +0200

    actor changes

--- src/main/java/pl/com/bottega/cinema/domain/Actor.java
+++ src/main/java/pl/com/bottega/cinema/domain/Actor.java
@@ -12,12 +12,10 @@ public class Actor {
     @Id
     @GeneratedValue
     private Long id;
-    private String firstName;
-    private String lastName;
+    private String name;
 
-    public Actor(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+    public Actor(String name) {
+        this.name = name;
     }
 
     public Long getId() {
@@ -28,19 +26,11 @@ public class Actor {
         this.id = id;
     }
 
-    public String getFirstName() {
-        return firstName;
+    public String getName() {
+        return name;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public void setName(String lastName) {
+        this.name = name;
     }
 }
