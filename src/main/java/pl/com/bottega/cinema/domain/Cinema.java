2cf3c3abfdc916c6666758f3f5cd680449e1578a
Bernard Boguszewski <bernardboguszewski@gmail.com>
2016-09-04 14:04:07 +0200

    JPACinemaRepository

--- src/main/java/pl/com/bottega/cinema/domain/Cinema.java
+++ src/main/java/pl/com/bottega/cinema/domain/Cinema.java
@@ -1,8 +1,10 @@
 package pl.com.bottega.cinema.domain;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Entity
 public class Cinema {
 
     @Id
