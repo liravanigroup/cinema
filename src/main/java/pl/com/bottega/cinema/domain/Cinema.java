2d2c922cd165efe47e6006054af158847e34733d
Admin <liravanigroup@gmail.com>
2016-09-04 11:54:51 +0200

    cinema equals()

--- src/main/java/pl/com/bottega/cinema/domain/Cinema.java
+++ src/main/java/pl/com/bottega/cinema/domain/Cinema.java
@@ -11,6 +11,25 @@ public class Cinema {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cinema cinema = (Cinema) o;
+
+        if (!city.equals(cinema.city)) return false;
+        return name.equals(cinema.name);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = city.hashCode();
+        result = 31 * result + name.hashCode();
+        return result;
+    }
+
+    @Override
     public String toString() {
         return "Cinema{" +
                 "city='" + city + '\'' +
