237ed9d025a0885b522480eb4af4f7abd4f67a01
Bernard Boguszewski <bernardboguszewski@gmail.com>
2016-09-04 11:43:55 +0200

    movie fields

--- src/main/java/pl/com/bottega/cinema/domain/Movie.java
+++ src/main/java/pl/com/bottega/cinema/domain/Movie.java
@@ -1,9 +1,17 @@
 package pl.com.bottega.cinema.domain;
 
+import java.util.List;
+
 /**
  * Created by anna on 04.09.2016.
  */
 public class Movie {
 
     private String title;
+    private String description;
+    private int minAge;
+    private List<String> actors;
+    private List<String> genres;
+    private int length;
+
 }
