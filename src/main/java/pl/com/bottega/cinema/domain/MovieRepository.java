d21d0bfec10322dcf7f94c1e8937fe3e69d19ca7
Bernard Boguszewski <bernardboguszewski@gmail.com>
2016-09-04 11:47:24 +0200

    MovieRepo and JPAMovieRepo

---
+++ src/main/java/pl/com/bottega/cinema/domain/MovieRepository.java
@@ -0,0 +1,7 @@
+package pl.com.bottega.cinema.domain;
+
+/**
+ * Created by bernard.boguszewski on 04.09.2016.
+ */
+public interface MovieRepository {
+}
---
+++ src/main/java/pl/com/bottega/cinema/infrastructure/JPAMoviesRepository.java
@@ -0,0 +1,7 @@
+package pl.com.bottega.cinema.infrastructure;
+
+/**
+ * Created by bernard.boguszewski on 04.09.2016.
+ */
+public class JPAMoviesRepository {
+}
