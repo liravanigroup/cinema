8ed85b44d515309067238dff56edf6bca17c0bb3
Admin <liravanigroup@gmail.com>
2016-09-04 12:58:29 +0200

    movie changes

--- src/main/java/pl/com/bottega/cinema/domain/Movie.java
+++ src/main/java/pl/com/bottega/cinema/domain/Movie.java
@@ -1,19 +1,91 @@
 package pl.com.bottega.cinema.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.util.List;
 
 /**
  * Created by anna on 04.09.2016.
  */
-
-
+@Entity
 public class Movie {
 
+    @Id
+    @GeneratedValue
+    private Long id;
     private String title;
     private String description;
     private Integer minAge;
-    private List<String> actors;
-    private List<String> genres;
-    private int length;
+    @OneToMany
+    private List<Actor> actors;
+    @OneToMany
+    private List<Genre> genres;
+    private Integer length;
 
+    public Movie(String title, String description, Integer minAge, List<Actor> actors, List<Genre> genres, Integer length) {
+        this.title = title;
+        this.description = description;
+        this.minAge = minAge;
+        this.actors = actors;
+        this.genres = genres;
+        this.length = length;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getMinAge() {
+        return minAge;
+    }
+
+    public void setMinAge(Integer minAge) {
+        this.minAge = minAge;
+    }
+
+    public List<Actor> getActors() {
+        return actors;
+    }
+
+    public void setActors(List<Actor> actors) {
+        this.actors = actors;
+    }
+
+    public List<Genre> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<Genre> genres) {
+        this.genres = genres;
+    }
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public void setLength(Integer length) {
+        this.length = length;
+    }
 }
