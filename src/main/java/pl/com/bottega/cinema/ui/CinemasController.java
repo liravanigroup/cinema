c3224e92ba240604bf63ef272464c9f94d02e03c
anna <krzysztoszek.a@gmail.com>
2016-09-04 12:48:36 +0200

    create classes in ui

---
+++ src/main/java/pl/com/bottega/cinema/ui/CinemasController.java
@@ -0,0 +1,17 @@
+package pl.com.bottega.cinema.ui;
+
+import pl.com.bottega.cinema.api.CreateCinemaRequest;
+
+/**
+ * Created by anna on 04.09.2016.
+ */
+public class CinemasController {
+
+    public void create(CreateCinemaRequest request) {
+
+    }
+
+    public ListAllCinemasResponse listAll() {
+        return null;
+    }
+}
