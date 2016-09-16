f7960dcb3fba89bd704d28571e535fcd226f6190
liravanigroup <liravanigroup@gmail.com>
2016-09-16 14:53:26 +0200

    5 task is working

--- src/main/java/pl/com/bottega/cinema/infrastructure/JPAMovieCatalog.java
+++ src/main/java/pl/com/bottega/cinema/infrastructure/JPAMovieCatalog.java
@@ -4,14 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pl.com.bottega.cinema.api.MovieCatalog;
 import pl.com.bottega.cinema.domain.Movie;
-import pl.com.bottega.cinema.domain.MovieRepository;
 import pl.com.bottega.cinema.domain.ShowsRepository;
 import pl.com.bottega.cinema.ui.ListMoviesResponse;
 import pl.com.bottega.cinema.ui.MovieResponseDto;
+import pl.com.bottega.cinema.ui.ShowData;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -33,7 +34,12 @@ public class JPAMovieCatalog implements MovieCatalog {
         checkNotNull(cinemaId);
         checkNotNull(date);
         List<Movie> movies = findAllMovieByCinemaIdAndDate(cinemaId, date);
-        List<MovieResponseDto> moviesResult = movies.stream().map(movie -> {
+        List<MovieResponseDto> moviesResult = getMovieResponseDtoList(cinemaId, date, movies);
+        return new ListMoviesResponse(moviesResult);
+    }
+
+    private List<MovieResponseDto> getMovieResponseDtoList(Long cinemaId, LocalDate date, List<Movie> movies) {
+        return movies.stream().map(movie -> {
             MovieResponseDto result = new MovieResponseDto();
             result.setTitle(movie.getTitle());
             result.setDescription(movie.getDescription());
@@ -41,14 +47,25 @@ public class JPAMovieCatalog implements MovieCatalog {
             result.setLength(movie.getLength());
             result.setActors(movie.getActors());
             result.setGenres(movie.getGenres());
-            result.setShows(showsRepository.load(cinemaId, movie.getId(), date));
+            result.setShows(getSortedShows(cinemaId, date, movie));
             return result;
+        }).sorted((o1, o2) -> o1.getTitle().compareTo(o2.getTitle())).collect(Collectors.toList());
+    }
+
+    private List<ShowData> getSortedShows(Long cinemaId, LocalDate date, Movie movie) {
+        return showsRepository.load(cinemaId, movie.getId(), date).stream().sorted((o1, o2) -> {
+            LocalTime time1 = o1.getTime();
+            LocalTime time2 = o2.getTime();
+            if (time1.isAfter(time2))
+                return 1;
+            if (time1.isBefore(time2))
+                return -1;
+            return 0;
         }).collect(Collectors.toList());
-        return new ListMoviesResponse(moviesResult);
     }
 
     private List<Movie> findAllMovieByCinemaIdAndDate(Long cinemaId, LocalDate date) {
-        return entityManager.createQuery("SELECT m FROM Movie m JOIN Show s ON m.id = s.movie.id JOIN Cinema c ON c.id = s.cinema.id WHERE c.id=:cinemaId AND s.date=:date", Movie.class)
+        return entityManager.createQuery("SELECT DISTINCT m FROM Movie m JOIN Show s ON m.id = s.movie.id JOIN Cinema c ON c.id = s.cinema.id WHERE c.id=:cinemaId AND s.date=:date", Movie.class)
                 .setParameter("cinemaId", cinemaId)
                 .setParameter("date", date)
                 .getResultList();
