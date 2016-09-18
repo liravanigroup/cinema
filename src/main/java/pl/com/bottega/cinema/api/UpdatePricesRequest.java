d3d90b505e2ee1676fe8ffc060eefdad1ceec23d
anna <krzysztoszek.a@gmail.com>
2016-09-18 11:42:00 +0200

    create classes

---
+++ src/main/java/pl/com/bottega/cinema/api/UpdatePricesRequest.java
@@ -0,0 +1,12 @@
+package pl.com.bottega.cinema.api;
+
+import java.util.Map;
+
+/**
+ * Created by anna on 18.09.2016.
+ */
+public class UpdatePricesRequest {
+
+    private Long movieId;
+    private Map prices;
+}
---
+++ src/main/java/pl/com/bottega/cinema/domain/TicketPrice.java
@@ -0,0 +1,12 @@
+package pl.com.bottega.cinema.domain;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by anna on 18.09.2016.
+ */
+public class TicketPrice {
+
+    private String type;
+    private BigDecimal price;
+}
