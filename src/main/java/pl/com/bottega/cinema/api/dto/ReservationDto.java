5fed145303adc4af1bcc084f16e57bcb5f28da5c
Admin <liravanigroup@gmail.com>
2016-09-25 12:34:44 +0200

    validate

--- src/main/java/pl/com/bottega/cinema/api/dto/ReservationDto.java
+++ src/main/java/pl/com/bottega/cinema/api/dto/ReservationDto.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 import pl.com.bottega.cinema.domain.TicketOrder;
 
+import java.math.BigDecimal;
 import java.util.Collection;
 
 import static pl.com.bottega.cinema.domain.validators.CollectionValidator.collectionValidate;
@@ -21,6 +22,7 @@ public class ReservationDto {
     private Collection<TicketOrder> tickets;
     private Collection<SeatDto> seats;
     private CustomerDto customer;
+    private BigDecimal totalPrice;
 
     public void validate(){
         entityIdValidate(showId, "Show id is required");
